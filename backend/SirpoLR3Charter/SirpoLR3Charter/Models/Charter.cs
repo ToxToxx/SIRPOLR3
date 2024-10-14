05315dcea0da52fb5e5dfd4b44019be0a1ae5fe7
Matvey <sinema3d00@gmail.com>
2024-10-14 11:11:26 +0300

    changes in model

--- backend/SirpoLR3Charter/SirpoLR3Charter/Models/Charter.cs
+++ backend/SirpoLR3Charter/SirpoLR3Charter/Models/Charter.cs
@@ -8,20 +8,12 @@ namespace SirpoLR3Charter.Models
         [DataMember]
         public Guid Id { get; init; }
         [DataMember]
-        public required string CititesPath { get; init; }
+        public string CititesPath { get; init; }
         [DataMember]
         public int Price { get; init; }
         [DataMember]
-        public DateTime DateTime { get; init; }
+        public DateTime CharterDateTime { get; init; }
         [DataMember]
         public DateTime CreatedAt { get; init; }
-
-        public Charter (string citiesPath, int price, DateTime dateTime)
-        {
-            CititesPath = citiesPath;
-            Price = price;
-            DateTime = dateTime;
-            CreatedAt = DateTime.Now;
-        }
     }
 }
